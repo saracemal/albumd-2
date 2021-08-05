70c81bc97f86b2b5312a4237ad5510dc192e3426
Sara Cemal <saracemal@gmail.com>
2021-08-05 19:21:03 -0400

    added sticky headers

--- src/FrontPageHeader.js
+++ src/FrontPageHeader.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useState, useEffect, useRef, useCallback } from "react";
 
-function FrontPageHeader() {
-    return (
-        <div>
-            front page header
-        </div>
-    )
-}
+const FrontPageHeader = (defaultSticky = false) => {
+    const [isSticky, setIsSticky] = useState(defaultSticky);
+    const tableRef = useRef(null);
+    const toggleSticky = useCallback(
+      ({ top, bottom }) => {
+        if (top <= 0 && bottom > 2 * 68) {
+          !isSticky && setIsSticky(true);
+        } else {
+          isSticky && setIsSticky(false);
+        }
+      },
+      [isSticky]
+    );
+    useEffect(() => {
+      const handleScroll = () => {
+        toggleSticky(tableRef.current.getBoundingClientRect());
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [toggleSticky]);
+    return { tableRef, isSticky };
+  };
 
 export default FrontPageHeader;
+
+
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useState, useEffect, useRef, useCallback } from "react";
 
-function Header() {
-    return (
-        <div>
-            This will be the header! 
-        </div>
-    )
-}
+const Header = (defaultSticky = false) => {
+    const [isSticky, setIsSticky] = useState(defaultSticky);
+    const tableRef = useRef(null);
+    const toggleSticky = useCallback(
+      ({ top, bottom }) => {
+        if (top <= 0 && bottom > 2 * 68) {
+          !isSticky && setIsSticky(true);
+        } else {
+          isSticky && setIsSticky(false);
+        }
+      },
+      [isSticky]
+    );
+    useEffect(() => {
+      const handleScroll = () => {
+        toggleSticky(tableRef.current.getBoundingClientRect());
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [toggleSticky]);
+    return { tableRef, isSticky };
+  };
 
 export default Header;
+
+

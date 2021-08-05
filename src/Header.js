e703bcf189855ed31ac2ba37d66f65ac846a5320
Sara Cemal <saracemal@gmail.com>
2021-08-05 19:09:03 -0400

    some additional components

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            This will be the header! 
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header.js';
+
+function LandingPage() {
+    return (
+        <div>
+            <Header />
+            This will be the landing page for all things album'd
+        </div>
+    )
+}
+
+export default LandingPage;

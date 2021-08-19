9db5b5f7aec121e1bef9482b74cc2e0caa803cb1
Sara Cemal <saracemal@gmail.com>
2021-08-19 18:46:38 -0400

    forgot about footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//will have my name, links to other social media, etc 
+function Footer() {
+    return (
+        <Container>
+            <h1>will add more to this later, </h1>
+        </Container>
+    )
+}
+
+export default Footer;

df4efd7e70370f871a9f5aa4ca4d7d213d7ee436
Sara Cemal <saracemal@gmail.com>
2021-08-19 18:48:13 -0400

    forgot styling for footer

--- src/Footer.js
+++ src/Footer.js
@@ -11,3 +11,12 @@ function Footer() {
 }
 
 export default Footer;
+
+const Container = styled.div`
+    background: rgba( 126, 211, 33, 0.35 );
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 5.0px );
+    -webkit-backdrop-filter: blur( 5.0px );
+    border-radius: 10px;
+    border: 1px solid rgba( 255, 255, 255, 0.18 );
+`
